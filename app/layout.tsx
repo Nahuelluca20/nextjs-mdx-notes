30ce5695286eb244dc55b7641e213aa1574bc6ac
Nahuel Luca <nahuelluca20@gmail.com>
2024-03-22 18:05:52 -0300

    setup darkmode

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import {Inter as FontSans} from "next/font/google";
 import "./globals.css";
 
 import {cn} from "@/lib/utils";
+import {ThemeProvider} from "@/providers/theme-provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -23,7 +24,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
-        {children}
+        <ThemeProvider
+          disableTransitionOnChange
+          enableSystem
+          attribute="class"
+          defaultTheme="system"
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
