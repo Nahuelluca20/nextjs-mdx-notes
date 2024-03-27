c08245035e4c37b20438b3fac2d97192a1b620af
Nahuel Luca <nahuelluca20@gmail.com>
2024-03-27 13:02:35 -0300

    Add Markdoc rendering and table of contents

--- app/dashboard/notes/[id]/page.tsx
+++ app/dashboard/notes/[id]/page.tsx
@@ -1,7 +1,44 @@
+import React from "react";
+import Markdoc from "@markdoc/markdoc";
+
 import {Badge} from "@/components/ui/badge";
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+import {components, config} from "@/lib/config.markdoc";
+import {extractHeadings} from "@/lib/extract-headings";
+import TableOfContents from "@/components/markdoc/table-of-contents";
 
 export default function Page({params}: {params: {id: string}}) {
+  const contentMDX = `
+  # Markdoc
+
+  Markdoc is a markdown parser and renderer for React. It is designed to be highly customizable and extensible.
+  - It supports custom components and attributes.
+
+  > This is a blockquote
+
+  ## Markdoc 2
+
+  Markdoc is a markdown parser and renderer for React. It is designed to be highly customizable and extensible.
+  - It supports custom components and attributes.
+
+  > This is a blockquoteMarkdoc is a markdown parser and renderer for React. It is designed to be highly customizable and extensible.
+  - It supports custom components and attributes.
+
+  > This is a blockquoteMarkdoc is a markdown parser and renderer for React. It is designed to be highly customizable and extensible.
+  - It supports custom components and attributes.
+
+  > This is a blockquoteMarkdoc is a markdown parser and renderer for React. It is designed to be highly customizable and extensible.
+  - It supports custom components and attributes.
+
+  > This is a blockquote
+
+
+  # Markdoc4
+  `;
+  const ast = Markdoc.parse(contentMDX);
+  const content = Markdoc.transform(ast, config);
+  const tableOfContents = extractHeadings(content);
+
   return (
     <Card className="w-full md:max-w-[320px] lg:max-w-[400px]">
       <CardHeader>
@@ -14,6 +51,8 @@ export default function Page({params}: {params: {id: string}}) {
       </CardHeader>
       <CardContent>
         <p>{params.id}</p>
+        <TableOfContents tableOfContents={tableOfContents} />
+        {Markdoc.renderers.react(content, React, {components})}
       </CardContent>
       {/* <CardFooter className="gap-2 flex justify-end">
         <Button className="flex items-center justify-between gap-2" variant={"secondary"}>
