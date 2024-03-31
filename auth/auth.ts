52114681fa1a71ff67394ec2fb683d8c81183f51
Nahuel Luca <nahuelluca20@gmail.com>
2024-03-31 16:12:05 -0300

    Remove Google and Email providers, add Github provider

--- auth/auth.ts
+++ auth/auth.ts
@@ -1,6 +1,4 @@
 import NextAuth from "next-auth";
-import Email from "next-auth/providers/email";
-import Google from "next-auth/providers/google";
 import Github from "next-auth/providers/github";
 
 import {authConfig} from "./auth.config";
@@ -13,27 +11,9 @@ export const {
 } = NextAuth({
   ...authConfig,
   providers: [
-    // Google({
-    //   clientId: process.env.AUTH_GOOGLE_ID,
-    //   clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    //   authorization: {
-    //     params: {
-    //       prompt: "consent",
-    //       access_type: "offline",
-    //       response_type: "code",
-    //     },
-    //   },
-    // }),
-    // Email({
-    //   server: {
-    //     host: process.env.SMTP_HOST,
-    //     port: process.env.SMTP_PORT,
-    //     auth: {
-    //       user: process.env.SMTP_USER,
-    //       pass: process.env.SMTP_PASSWORD,
-    //     },
-    //   },
-    //   from: process.env.EMAIL_FROM,
-    // }),
+    Github({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
   ],
 });
