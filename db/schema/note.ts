bd569aa98c599f1052731dab5af9fd91fb4953fe
Nahuel Luca <nahuelluca20@gmail.com>
2024-04-01 17:56:00 -0300

    Add userId column to note table

--- db/schema/note.ts
+++ db/schema/note.ts
@@ -1,10 +1,15 @@
 import {text, sqliteTable} from "drizzle-orm/sqlite-core";
 import {createId} from "@paralleldrive/cuid2";
 
+import {users} from "./user";
+
 export const note = sqliteTable("note", {
   id: text("id").$defaultFn(() => createId()),
   title: text("title"),
   description: text("description"),
   tags: text("tags"),
   content: text("content"),
+  userId: text("userId")
+    .notNull()
+    .references(() => users.id, {onDelete: "cascade"}),
 });
