029eb34b133c416f62ed473f369f2a8488dfdbb6
Nahuel Luca <nahuelluca20@gmail.com>
2024-04-03 11:45:10 -0300

    create initial page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,26 @@
 import Link from "next/link";
 
-import {ModeToggle} from "@/components/buttons/mode-toggle";
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <ModeToggle />
-        <Link href="/dashboard">Dashboard</Link>
+      <main className="text-center flex min-h-screen flex-col items-center gap-8 p-10 md:p-24">
+        <h1 className="text-5xl font-bold">MDX Notes</h1>
+        <h3 className="max-w-[600px] text-xl text-muted-foreground font-semibold">
+          This is an app for creating MDX Notes. It is built with NextJS, Shadcn/ui, Markdoc,
+          Drizzle ORM, Turso as database and authjs/next-auth for authentication.
+        </h3>
+        <div className="space-x-2">
+          <Button asChild>
+            <Link href="/dashboard">Go to Create</Link>
+          </Button>
+          <Button asChild variant={"secondary"}>
+            <Link href="https://github.com/Nahuelluca20/nextjs-mdx-notes" target="_blank">
+              See Code
+            </Link>
+          </Button>
+        </div>
       </main>
     </>
   );
